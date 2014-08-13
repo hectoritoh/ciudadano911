c02398d86cbf5a9b76ac0cea31de8f7df0a6f613
Hector Alvarado <halvarad@fiec.espol.edu.ec>
2014-08-13 01:18:22 -0500

    creacion de controlador de servicios

---
+++ src/Celmedia/AdminThemeBundle/Controller/ServiciosController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Celmedia\AdminThemeBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Celmedia\PortalBundle\Entity\Campana;
+use Celmedia\PortalBundle\Entity\Mensaje;
+
+class DefaultController extends Controller
+{
+	
+
+/*
+implementar y que devuelva json
+*/
+public function getDirectoriosAction()
+{
+    
+}
+
+
+
+}
