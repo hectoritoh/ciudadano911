9918836b38a643575721662a11bacfde587205d0
Fabricio Orrala <orrala@celmedia.com>
2014-08-13 02:23:59 -0500

    emergencias

--- src/Celmedia/AdminThemeBundle/Controller/ServiciosController.php
+++ src/Celmedia/AdminThemeBundle/Controller/ServiciosController.php
@@ -5,21 +5,25 @@ namespace Celmedia\AdminThemeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Celmedia\PortalBundle\Entity\Campana;
 use Celmedia\PortalBundle\Entity\Mensaje;
 
-class DefaultController extends Controller
+class ServiciosController extends Controller
 {
 	
 
-/*
-implementar y que devuelva json
-*/
-public function getDirectoriosAction()
-{
-    
-}
+	/*
+	implementar y que devuelva json
+	*/
+	public function getDirectoriosAction()
+	{
+	    return new JsonResponse(array(
+            'codigo' => 0,
+            'mensaje' => "No se encontraron directorios"
+        ), 200); //codigo de error diferente
+	}
 
 
 
--- src/Celmedia/AdminThemeBundle/Resources/public/js/emergencias.js
+++ src/Celmedia/AdminThemeBundle/Resources/public/js/emergencias.js
@@ -29,75 +29,76 @@ $(document).ready(function() {
 
     emergenciasMapa = obtenerPuntosBase();
 
-    console.log("arreglo: " + emergenciasMapa[0]["tipoEmergencia"]);
-    
+    if( emergenciasMapa.length > 0 ){
+    	var marcadorAzul = new google.maps.MarkerImage("../../web/bundles/celmediaadmintheme/img/puntero-azul.png",
+    	    new google.maps.Size(50,50),
+    	    new google.maps.Point(0,0),
+    	    new google.maps.Point(50,50)
+    	);
 
-	var marcadorAzul = new google.maps.MarkerImage("../../web/bundles/celmediaadmintheme/img/puntero-azul.png",
-	    new google.maps.Size(50,50),
-	    new google.maps.Point(0,0),
-	    new google.maps.Point(50,50)
-	);
+    	var marcadorRojo = new google.maps.MarkerImage("../../web/bundles/celmediaadmintheme/img/puntero-rojo.png",
+    	    new google.maps.Size(50,50),
+    	    new google.maps.Point(0,0),
+    	    new google.maps.Point(50,50)
+    	);
 
-	var marcadorRojo = new google.maps.MarkerImage("../../web/bundles/celmediaadmintheme/img/puntero-rojo.png",
-	    new google.maps.Size(50,50),
-	    new google.maps.Point(0,0),
-	    new google.maps.Point(50,50)
-	);
+    	//////////////////////////////////////////////////////////////
 
-	//////////////////////////////////////////////////////////////
+        var map;
 
-    var map;
+        var zona0 = new google.maps.LatLng(emergenciasMapa[0]["latitud"],emergenciasMapa[0]["longitud"]);
 
-    var zona0 = new google.maps.LatLng(emergenciasMapa[0]["latitud"],emergenciasMapa[0]["longitud"]);
+        var stylez = [
+            {
+                featureType: "all",
+                elementType: "all",
+                stylers: [
+                    {saturation: -100} // <-- THIS
+                ]
+            }
+        ];
 
-    var stylez = [
-        {
-            featureType: "all",
-            elementType: "all",
-            stylers: [
-                {saturation: -100} // <-- THIS
-            ]
-        }
-    ];
+        var mapOptions = {
+        	zoom: 15,
+            zoomControl: true,
+            scaleControl: false,
+            scrollwheel: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            disableDefaultUI: true,
+            center: zona0,
+            mapTypeControlOptions: {
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'tehgrayz']
+            }
+        };
 
-    var mapOptions = {
-    	zoom: 8,
-        zoomControl: true,
-        scaleControl: false,
-        scrollwheel: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true,
-        center: zona0,
-        mapTypeControlOptions: {
-            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'tehgrayz']
-        }
-    };
+        map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
-    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+        for (var i = 0; i < emergenciasMapa.length; i++) {
+        	
+        	var ubicacion = new google.maps.LatLng(emergenciasMapa[i]["latitud"], emergenciasMapa[i]["longitud"]);
 
-    for (var i = 0; i < emergenciasMapa.length; i++) {
-    	
-    	var ubicacion = new google.maps.LatLng(emergenciasMapa[i]["latitud"], emergenciasMapa[i]["longitud"]);
+        	var icono;
+        	if(i%2 == 0){
+        		icono = marcadorAzul;
+        	}else{
+        		icono = marcadorRojo;
+        	}
 
-    	var icono;
-    	if(i%2 == 0){
-    		icono = marcadorAzul;
-    	}else{
-    		icono = marcadorRojo;
+    		var marcador = new google.maps.Marker({
+    			position: ubicacion,
+    			map: map,
+    			icon: icono,
+    			title: emergenciasMapa[i]["tipoEmergencia"],
+    			zIndex: 3
+    		});
     	}
 
-		var marcador = new google.maps.Marker({
-			position: ubicacion,
-			map: map,
-			icon: icono,
-			title: emergenciasMapa[i]["tipoEmergencia"],
-			zIndex: 3
-		});
-	}
+        var mapType = new google.maps.StyledMapType(stylez, {name: "Emergencias"});
+        map.mapTypes.set('tehgrayz', mapType);
+        map.setMapTypeId('tehgrayz');
+        
+    }
 
-    var mapType = new google.maps.StyledMapType(stylez, {name: "Emergencias"});
-    map.mapTypes.set('tehgrayz', mapType);
-    map.setMapTypeId('tehgrayz');
 });
 
 
