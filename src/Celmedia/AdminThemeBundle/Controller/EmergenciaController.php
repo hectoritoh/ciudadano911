1ff58bb0b4722ad4f105abb16c625511e3950344
Fabricio Orrala <orrala@celmedia.com>
2014-08-13 01:57:16 -0500

    mapa de emergencias version 0.01

--- src/Celmedia/AdminThemeBundle/Controller/EmergenciaController.php
+++ src/Celmedia/AdminThemeBundle/Controller/EmergenciaController.php
@@ -5,6 +5,7 @@ namespace Celmedia\AdminThemeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 use Celmedia\PortalBundle\Entity\Campana;
 use Celmedia\PortalBundle\Entity\Mensaje;
@@ -13,7 +14,6 @@ class EmergenciaController extends Controller
 {
 	
 
-
 	public function emergenciasAction()
 	{
 		return $this->render('CelmediaAdminThemeBundle:Pages:emergencias.html.twig');
@@ -21,8 +21,41 @@ class EmergenciaController extends Controller
 
 
 
+	
+
+	public function getEmergenciasAction()
+	{
+		$em = $this->getDoctrine()->getManager();        
+        
+        $emergencias = $this->getDoctrine()->getRepository("AppCoreBundle:Emergencia")->findAll();
+
+        if(count($emergencias) > 0){
+	        $arrEmergencias = array();
+
+	        foreach ($emergencias as $emergencia) {
+	        	array_push($arrEmergencias, array(
+	        		'id' => $emergencia->getId(),
+	        		'tipoEmergencia' => $emergencia->getTipoEmergencia(),
+	        		'latitud' => $emergencia->getLatitud(),
+	        		'longitud' => $emergencia->getLonguitud()
+	        		)
+	        	);
+	        }
 
 
+	        return new JsonResponse(array(
+	            'codigo' => 1,
+	            'mensaje' => "Emergencias encontradas",
+	            'listaEmergencias' => $arrEmergencias
+	        ), 200); //codigo de error diferente
+        	
+        }else{
+        	return new JsonResponse(array(
+	            'codigo' => 0,
+	            'mensaje' => "No se encontraron emergencias"
+	        ), 200); //codigo de error diferente
+        }
+	}
 
 
 }
---
+++ src/Celmedia/AdminThemeBundle/Resources/public/js/emergencias.js
@@ -0,0 +1,104 @@
+var emergenciasMapa = new Array();
+
+function obtenerPuntosBase(){
+    var puntosMapa = new Array();
+
+    $.ajax({
+        url: Routing.generate('get_emergencias'),
+        type: 'POST',
+        async: false,
+        //data: parametros,
+        dataType: "json",
+        success: function (respuesta) {
+          if(respuesta.codigo == 1){
+            console.log("Okay: " + respuesta.mensaje);
+            puntosMapa = respuesta.listaEmergencias;
+          }else{
+            console.log("Error: " + respuesta.mensaje);
+          }
+        }, 
+        error: function (error) {
+          console.log("ERROR: " + error);
+        }
+    });
+
+    return puntosMapa;
+}
+
+$(document).ready(function() {
+
+    emergenciasMapa = obtenerPuntosBase();
+
+    console.log("arreglo: " + emergenciasMapa[0]["tipoEmergencia"]);
+    
+
+	var marcadorAzul = new google.maps.MarkerImage("../../web/bundles/celmediaadmintheme/img/puntero-azul.png",
+	    new google.maps.Size(50,50),
+	    new google.maps.Point(0,0),
+	    new google.maps.Point(50,50)
+	);
+
+	var marcadorRojo = new google.maps.MarkerImage("../../web/bundles/celmediaadmintheme/img/puntero-rojo.png",
+	    new google.maps.Size(50,50),
+	    new google.maps.Point(0,0),
+	    new google.maps.Point(50,50)
+	);
+
+	//////////////////////////////////////////////////////////////
+
+    var map;
+
+    var zona0 = new google.maps.LatLng(emergenciasMapa[0]["latitud"],emergenciasMapa[0]["longitud"]);
+
+    var stylez = [
+        {
+            featureType: "all",
+            elementType: "all",
+            stylers: [
+                {saturation: -100} // <-- THIS
+            ]
+        }
+    ];
+
+    var mapOptions = {
+    	zoom: 8,
+        zoomControl: true,
+        scaleControl: false,
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        center: zona0,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'tehgrayz']
+        }
+    };
+
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+    for (var i = 0; i < emergenciasMapa.length; i++) {
+    	
+    	var ubicacion = new google.maps.LatLng(emergenciasMapa[i]["latitud"], emergenciasMapa[i]["longitud"]);
+
+    	var icono;
+    	if(i%2 == 0){
+    		icono = marcadorAzul;
+    	}else{
+    		icono = marcadorRojo;
+    	}
+
+		var marcador = new google.maps.Marker({
+			position: ubicacion,
+			map: map,
+			icon: icono,
+			title: emergenciasMapa[i]["tipoEmergencia"],
+			zIndex: 3
+		});
+	}
+
+    var mapType = new google.maps.StyledMapType(stylez, {name: "Emergencias"});
+    map.mapTypes.set('tehgrayz', mapType);
+    map.setMapTypeId('tehgrayz');
+});
+
+
+
